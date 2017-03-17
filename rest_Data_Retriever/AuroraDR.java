787862b97560d52ba03bd6bf9706f737c3953ab6
Elijah Pennoyer <elijah.pennoyer@gmail.com>
2017-03-16 21:37:36 -0600

    Revert "Revert "Maybe working for Milestone 2 thanks to Raj""
    This reverts commit 838833db3973b9972bc1ce332b5bbe251d0d5d74.

--- rest_Data_Retriever/AuroraDR.java
+++ rest_Data_Retriever/AuroraDR.java
@@ -1,14 +1,16 @@
 package rest_Data_Retriever;
 
+import java.io.InputStream;
+import java.security.InvalidParameterException;
+
+import javax.ws.rs.core.Response;
+
 import org.json.JSONArray;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import java.io.InputStream;
-
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 /**
  * receive uri from data controller. send http request to aurora api. receive api response.
@@ -42,10 +44,22 @@ public class AuroraDR {
 	 * @throws UnirestException If there was an error when sending the http request to auroras.live
 	 */
 	public static Response auroraAPI_ImageRetriever(String URI) throws UnirestException{
+		
 		HttpResponse<InputStream> response =
-		Unirest.get("http://api.auroras.live/v1/" + URI)
+		Unirest.get("http://api.auroras.live/v1/?" + URI)
 		 .header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asBinary();
-		return Response.status(200).entity(response.getBody()).build();
+		
+		int status = response.getStatus();
+		
+		if(status == 200){
+			return Response.status(200).type("image/jpeg").entity(response.getBody()).build();
+		}
+		else if(status == 404){
+			return Response.status(404).type("application/json").entity(response.getBody()).build();
+		}
+		else{
+			throw new InvalidParameterException("Unexpected return status from Aurora API");
+		}
 	}
 	
 	/**
@@ -54,15 +68,16 @@ public class AuroraDR {
 	 * @return auroras.live API response
 	 * @throws UnirestException If there was an error when sending the http request to auroras.live
 	 */
+	//@Produces("application/json")
 	public static Response auroraAPI_JSONRetriever(String URI) throws UnirestException{
 		JSONArray jsonArray = new JSONArray();
 		HttpResponse<JsonNode> response =
-		Unirest.get("http://api.auroras.live/v1/" + URI)
+		Unirest.get("http://api.auroras.live/v1/?" + URI)
 		 .header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0")
 		 .asJson();
 		jsonArray = response.getBody().getArray();
 		String att = "Powered by Auroras.live";
 		jsonArray.getJSONObject(jsonArray.length()-1).put("Attribution", att);
-		return Response.status(200).entity(response.getBody().toString()).build();
+		return Response.status(200).type("application/json").entity(response.getBody().toString()).build();
 	}
 }

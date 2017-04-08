a8267bee5188880478e3191ae99fb48df860d7b3
Carguy18 <aaronkobelsky@gmail.com>
2017-04-08 13:57:48 -0600

    Added full comments and bugfix for no "type" parameter server crash

--- rest_Request_Handler/RequestHandler.java
+++ rest_Request_Handler/RequestHandler.java
@@ -1,4 +1,5 @@
 package rest_Request_Handler;
+
 import java.util.HashMap;
 import java.util.List;
 import javax.ws.rs.GET;
@@ -27,42 +28,70 @@ public class RequestHandler{
 	@Produces({"image/jpeg", "application/json"})
 	public Response handleRequest(@Context UriInfo uriInfo){
 		
+		//if the cache has not yet been created
+		//this statement block will run exactly once on server startup
 		if(!cache_Controller.DataController.cacheGoodToGo){
+			//create a new HashMap to be used as the cache
 			cache_Controller.DataController.cache = new HashMap<String, Object[]>();
 			cache_Controller.DataController.cacheGoodToGo = true;
 		}
 		
+		//retreive the entire URI query (ex. type=XXX&action=YYY&ZZZ=AAA)
 		String query = uriInfo.getRequestUri().getQuery();
 		
+		//parse the URI into its component query parameters (ex. type=XXX)
 		MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+		//retreive all parameters corresponding to "type"
 		List<String> typeList = queryParameters.get("type");
+		//error check the user has entered a type parameter
+		if(typeList.isEmpty() || typeList == null){
+			//return error message to the user detailing that they have not enetered any "type" parameters
+			JSONObject jsonObject = new JSONObject();
+			String module = "Main";
+			jsonObject.put("module", module);
+			String message = "Parameter is missing from request: type";
+			jsonObject.put("message", message);
+			int status = 404;
+			jsonObject.put("statuscode", status);
+			return Response.status(200).type("application/json").entity(jsonObject.toString()).build();
+		}
+		//retreive the last type parameter input by the user
 		String type = typeList.get(typeList.size()-1);
 		
+		//if the return type is expected to be an image
 		if(type.equals("embed") || (type.equals("images") && !queryParameters.containsKey("action"))){
+			//if the user has specified a no caching parameter
 			if(queryParameters.containsKey("no-caching")){
+				//retreive the last no-caching parameter specified by the user
 				List<String> noCacheList = queryParameters.get("no-caching");
 				String noCache = null;
 				if(noCacheList != null && !noCacheList.isEmpty()){
 					noCache = noCacheList.get(noCacheList.size()-1);
 					
+					//if the retreived parameter specifies "true"
+					//Call DataController.retrieveAuroraImage to retrieve the data by making a request to an API.
 					if(noCache.equals("true")){
 						return cache_Controller.DataController.retrieveAuroraImage(query, false);
 					}
 				}
 			}
-			//Call DataController.retrieveAuroraImage to retrieve the data, either from the Database 
-			//or by making a request to an API if the data isn't in the Database.
+			//if no "no-caching" parameter was given by the user or "no-caching" was not specified as "true"
+			//Call DataController.retrieveAuroraImage to retrieve the data, either from the cache 
+			//or by making a request to an API if the data isn't in the cache.
 			return cache_Controller.DataController.retrieveAuroraImage(query, true);	
 		}
+		
+		//if the expected return type is a map image
 		else if(type.equals("map")){
+			//get the last "id" query parameter entered by the user
 			List<String> idList = queryParameters.get("id");
 			String id = null;
 			if(idList != null && !idList.isEmpty()){
 				id = idList.get(idList.size()-1);
 			}
+			//if no "id" parameter was specified
 			else{
-				//TODO: maybe make this in order
-				//return a json object explaining to the user that it is NOT ok to not provide a location id
+				//return error message to the user detailing that they have not enetered any "id" parameters
 				JSONObject jsonObject = new JSONObject();
 				String module = "Map";
 				jsonObject.put("module", module);
@@ -72,31 +101,47 @@ public class RequestHandler{
 				jsonObject.put("statuscode", status);
 				return Response.status(200).type("application/json").entity(jsonObject.toString()).build();
 			}
+			//if the user specified a "no-caching" parameter
 			if(queryParameters.containsKey("no-caching")){
+				//get the last "no-caching" parameter entered by the user
 				List<String> cacheList = queryParameters.get("no-caching");
 				String cache = null;
 				if(cacheList != null && !cacheList.isEmpty()){
 					cache = cacheList.get(cacheList.size()-1);
 					
+					//if the retreived parameter has been specified as "true"
+					//Call DataController.retrieveMap to retrieve the data by making a request to an API.
 					if(cache.equals("true")){
 						return cache_Controller.DataController.retrieveMap(id, false);
 					}
 				}
 			}
+			//if no "no-caching" parameter was given by the user or "no-caching" was not specified as "true"
+			//Call DataController.retrieveMap to retrieve the data, either from the cache 
+			//or by making a request to an API if the data isn't in the cache.
 			return cache_Controller.DataController.retrieveMap(id, true);
 		}
+		
+		//if the expected return type is a JSON object
 		else{
+			//if the user specified a "no-caching" parameter
 			if(queryParameters.containsKey("no-caching")){
+				//get the last "no-caching" parameter entered by the user
 				List<String> cacheList = queryParameters.get("no-caching");
 				String cache = null;
 				if(cacheList != null && !cacheList.isEmpty()){
 					cache = cacheList.get(cacheList.size()-1);
 					
+					//if the retreived parameter has been specified as "true"
+					//Call DataController.retrieveAuroraJSON to retrieve the data by making a request to an API.
 					if(cache.equals("true")){
 						return cache_Controller.DataController.retrieveAuroraJSON(query, false);
 					}
 				}
 			}
+			//if no "no-caching" parameter was given by the user or "no-caching" was not specified as "true"
+			//Call DataController.retrieveAuroraJSON to retrieve the data, either from the cache 
+			//or by making a request to an API if the data isn't in the cache.
 			return cache_Controller.DataController.retrieveAuroraJSON(query, true);
 		}
 		
